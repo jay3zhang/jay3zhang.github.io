896f5fbeac249a161bf0e3d577dd73ffcd261fbd
jay3zhang <jay_1121863910@163.com>
2016-09-09 09:24:04 +0800

    Update Stage.js

--- greeting_card/Stage.js
+++ greeting_card/Stage.js
@@ -4,8 +4,8 @@ function Stage () {
 	s.width = canvasTag.width;
 	s.height = canvasTag.height;
 	s.bgColor = ctx.createRadialGradient(s.width / 2, s.height / 2, 10, s.width / 2, s.height / 2, s.width * 0.6);
-	//s.bgColor.addColorStop(0.3, "#CCCCCC");
-	s.bgColor.addColorStop(0.3, "#000000");
+	s.bgColor.addColorStop(0.3, "#CCCCCC");
+	
 	s.bgColor.addColorStop(1.0, "#FFFFFF");
 }
 
